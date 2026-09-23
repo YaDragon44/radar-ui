c94eb79a2f4534e900b3e79733411440f520aa24
YaDragon44 <7jr44jtzvy@privaterelay.appleid.com>
2026-09-23 14:21:57 +0300

    PB-5C publish real Idea pilot data

---
+++ ideas-data.js
@@ -0,0 +1,23 @@
+window.RADAR_IDEAS = [
+  {
+    type: "IDEA",
+    version: "1.0",
+    semantic_class: "ANALYSIS",
+    status: "PUBLISHED",
+    asset_id: "MOEX:TQBR:TATN",
+    ticker: "TATN",
+    title: "Татнефть : на столе сценарий увеличения нормы выплаты дивидендов до 75%",
+    published_at: "2026-09-02",
+    horizon_end: "2027-09-02",
+    potential_return_pct: 22.55,
+    current_return_pct: 8.09,
+    quality: "LIVE",
+    source: {
+      system: "T_INVESTMENTS_PUBLIC_IDEAS",
+      provider: "ЛМС",
+      ref: "https://www.tbank.ru/invest/stocks/TATN/ideas/"
+    },
+    action: null,
+    radar_decision: "N/A"
+  }
+];
